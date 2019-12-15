f111921a8520413c2cb2cf29bcf2a0e61cbf38c3
omer <amsteromer@gmail.com>
2019-12-15 02:58:22 +0200

    s

--- app/program.py
+++ app/program.py
@@ -2,12 +2,10 @@ from time import sleep
 import settings
 from crawler import PasteCrawler
 
-from app.settings import CRAWLER_SECONDS_DELAY
-
 while True:
     print("Stared Crawling...")
     storage_handler = settings.get_storage_handler()
     paste_crawler = PasteCrawler(storage_handler)
     paste_crawler.crawl()
-    print("Finished Crawling, Waiting {} seconds".format(CRAWLER_SECONDS_DELAY))
-    sleep(CRAWLER_SECONDS_DELAY)
+    print("Finished Crawling, Waiting {} seconds".format(settings.CRAWLER_SECONDS_DELAY))
+    sleep(settings.CRAWLER_SECONDS_DELAY)
